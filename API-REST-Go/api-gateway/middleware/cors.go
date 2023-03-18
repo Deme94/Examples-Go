383bf6aa9ef3bee9b97af95d1de971e93f5d6719
Deme94 <deme1994@gmail.com>
2023-03-18 19:18:11 +0100

    update cors middleware: add allowed headers to .env

--- API-REST-Go/api-gateway/middleware/cors.go
+++ API-REST-Go/api-gateway/middleware/cors.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"API-REST/services/conf"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func CORS() func(*fiber.Ctx) error {
 	return cors.New(cors.Config{
-		AllowOrigins: "*",
+		AllowOrigins: conf.Env.GetString("CORS_ORIGINS"),
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 	})
 }
